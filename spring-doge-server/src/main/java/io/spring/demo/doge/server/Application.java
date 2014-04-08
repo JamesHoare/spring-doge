6062582ba560b70005abe6af0e3ce8d5c12d0cc5
Josh Long <josh@joshlong.com>
2014-04-08 03:53:00 -0700

    spent way too much time on this

--- spring-doge-server/src/main/java/io/spring/demo/doge/server/Application.java
+++ spring-doge-server/src/main/java/io/spring/demo/doge/server/Application.java
@@ -16,16 +16,24 @@
 
 package io.spring.demo.doge.server;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import io.spring.demo.doge.photo.manipulate.DogePhotoManipulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.embedded.MultiPartConfigFactory;
-import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -41,6 +49,10 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 import javax.servlet.MultipartConfigElement;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Application configuration and main method.
@@ -66,19 +78,67 @@ public class Application {
     }
 
     @Bean
-    public GridFsTemplate gridFsTemplate(Mongo mongo, MongoTemplate mongoTemplate) {
-        return new GridFsTemplate(new SimpleMongoDbFactory(mongo, "fs"),
+    public GridFsTemplate gridFsTemplate(MongoDbFactory mongoDbFactory, MongoTemplate mongoTemplate) {
+        return new GridFsTemplate(mongoDbFactory,
                 mongoTemplate.getConverter());
+    }
+
+/*
+
+    @Bean
+    public MongoDbFactory mongoDbFactory(Mongo mongo) {
+        return new SimpleMongoDbFactory(mongo, "doge");
+    } 
+    @Bean
+    MongoClient mongoClient (  Environment environment ) throws IOException
+    {
+         // VCAP_SERVICES={"mongolab":[{"name":"doge-mongodb","label":"mongolab","tags":["document","mongodb","Data Store"],"plan":"sandbox","credentials":{"uri":"mongodb://CloudFoundry_brfvsjks_m6ga9qrt_bfom35f8:8n41doAsVbviQulOVPAdGKTm8HaX7Vm1@ds043047.mongolab.com:43047/CloudFoundry_brfvsjks_m6ga9qrt"}}]}
+
+
+
+        String creds =   environment.getProperty( "vcap.services.doge-mongodb.credentials" );
+
+        System.out.println( "credentials for MongoLab MongoDB client: " + creds) ;
+        URI uri  = URI.create( creds) ;
+
+        ServerAddress serverAddress = new ServerAddress( uri.getHost(), uri.getPort()) ;
+        MongoClient mongoClient =new MongoClient( serverAddress, Arrays.asList(  MongoCredential.createMongoCRCredential( uri.getUserInfo(), ur.getUser ))) ;
+
+    }*/
+
+/*
+    @Configuration
+    @Profile("default")
+    public static class DefaultConfiguration {
+
+
     }
 
     @Configuration
     @Profile("cloud")
-    public static class CloudConfiguration extends AbstractCloudConfig {
-        @Bean
-        public MongoDbFactory documentMongoDbFactory() {
-            return connectionFactory().mongoDbFactory();
+    //@EnableConfigurationProperties(MongoProperties.class)
+    public static class CloudConfiguration {
+
+  *//*      @Bean
+        public Cloud cloud() {
+            return new CloudFactory().getCloud();
+        }*//*
+
+        {
+
         }
-    }
+        String mongoUri =
+                "mongodb://CloudFoundry_brfvsjks_m6ga9qrt_lppt1ejo:jeTuhiPKR7H7JWvFJbjy2monmwaEF5GG@ds043047.mongolab.com:43047/CloudFoundry_brfvsjks_m6ga9qrt";
+
+        @Bean
+        public Mongo mongo() throws Exception {
+            List<MongoCredential> mongoCredentials = Arrays.asList(MongoCredential.createMongoCRCredential(
+                    "CloudFoundry_brfvsjks_m6ga9qrt_lppt1ejo", "CloudFoundry_brfvsjks_m6ga9qrt", "jeTuhiPKR7H7JWvFJbjy2monmwaEF5GG".toCharArray()));
+            return new MongoClient(new ServerAddress("ds043047.mongolab.com", 43047), mongoCredentials);
+        }
+
+
+    }*/
 
     @Configuration
     @EnableScheduling
